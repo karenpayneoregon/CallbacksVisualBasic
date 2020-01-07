045f20e1ed58fbb7b5f05b94ed6c90443b3fc8a6
Karen Payne <paynekaren@comcast.net>
2020-01-06 19:24:12 -0800

    Added code for another person's tech net article

---
+++ PassingValuesConventional/ChildForm.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ChildForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.LastNameTextBox = New System.Windows.Forms.TextBox()
+        Me.OkayButton = New System.Windows.Forms.Button()
+        Me.CancelButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(26, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "First"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(27, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Last"
+        '
+        'FirstNameTextBox
+        '
+        Me.FirstNameTextBox.Location = New System.Drawing.Point(65, 29)
+        Me.FirstNameTextBox.Name = "FirstNameTextBox"
+        Me.FirstNameTextBox.Size = New System.Drawing.Size(127, 20)
+        Me.FirstNameTextBox.TabIndex = 2
+        '
+        'LastNameTextBox
+        '
+        Me.LastNameTextBox.Location = New System.Drawing.Point(65, 58)
+        Me.LastNameTextBox.Name = "LastNameTextBox"
+        Me.LastNameTextBox.Size = New System.Drawing.Size(127, 20)
+        Me.LastNameTextBox.TabIndex = 3
+        '
+        'OkayButton
+        '
+        Me.OkayButton.Location = New System.Drawing.Point(19, 92)
+        Me.OkayButton.Name = "OkayButton"
+        Me.OkayButton.Size = New System.Drawing.Size(75, 23)
+        Me.OkayButton.TabIndex = 4
+        Me.OkayButton.Text = "Accept"
+        Me.OkayButton.UseVisualStyleBackColor = True
+        '
+        'CancelButton
+        '
+        Me.CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.CancelButton.Location = New System.Drawing.Point(117, 92)
+        Me.CancelButton.Name = "CancelButton"
+        Me.CancelButton.Size = New System.Drawing.Size(75, 23)
+        Me.CancelButton.TabIndex = 5
+        Me.CancelButton.Text = "Cancel"
+        Me.CancelButton.UseVisualStyleBackColor = True
+        '
+        'ChildForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(239, 131)
+        Me.Controls.Add(Me.CancelButton)
+        Me.Controls.Add(Me.OkayButton)
+        Me.Controls.Add(Me.LastNameTextBox)
+        Me.Controls.Add(Me.FirstNameTextBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "ChildForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Child"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents FirstNameTextBox As TextBox
+    Friend WithEvents LastNameTextBox As TextBox
+    Friend WithEvents OkayButton As Button
+    Friend WithEvents CancelButton As Button
+End Class
---
+++ PassingValuesConventional/Form1.vb
@@ -0,0 +1,16 @@
+﻿Imports PassingValuesConventional.Classes
+
+Public Class Form1
+    Private _person As New Person
+    Private Sub ChildFormButton1_Click(sender As Object, e As EventArgs) Handles ChildFormButton1.Click
+        If Not String.IsNullOrWhiteSpace(FirstNameTextBox.Text) AndAlso Not String.IsNullOrWhiteSpace(LastNameTextBox.Text) Then
+            _person.FirstName = FirstNameTextBox.Text
+            _person.LastName = LastNameTextBox.Text
+            Dim childForm As New ChildForm(_person)
+            If childForm.ShowDialog() = DialogResult.OK Then
+                FirstNameTextBox.Text = childForm.Person.FirstName
+                LastNameTextBox.Text = childForm.Person.LastName
+            End If
+        End If
+    End Sub
+End Class
